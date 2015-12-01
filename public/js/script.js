5e849107e70be55f53029efb3400fb7678d67e12
juanmanuel chaguendo bermeo <juanman90@gmail.com>
2015-11-30 22:39:04 -0500

    update canvas and object to move and draw elements

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
 	//$.extend(window,{});
 	//document.addEventListener("keydown", function (ev){
 	$(this).keydown(function (ev) {	
-		ev.preventDefault();
-		if (ev.keyCode == 38) {
+		if (ev.keyCode === 38) {
+			ev.preventDefault();
 			bar.up();
-		} else if (ev.keyCode == 40) {
+		} else if (ev.keyCode === 40) {
+			ev.preventDefault();
 			bar.down();
-		} else if (ev.keyCode == 87) {
+		} else if (ev.keyCode === 87) {
+			ev.preventDefault();
 			bar_2.up();
-		} else if (ev.keyCode == 83) {
+		} else if (ev.keyCode === 83) {
+			ev.preventDefault();
 			bar_2.down();
+		} else if (ev.keyCode === 32 ) {
+			ev.preventDefault();
+			board.playing = !board.playing;
 		}
 	});
 });
@@ -25,12 +31,13 @@ $(document).ready(function() {
 		this.game_over = false;
 		this.bars = [];
 		this.ball = null;
+		this.playing = false;
 	};
 
 	self.Board.prototype = {
 		get elements() {
-			var elements = this.bars;
-			//elements.push(this.ball);
+			var elements = this.bars.map(function(bar){return bar;});
+			elements.push(this.ball);
 			return elements;
 		}
 	};
@@ -46,7 +53,7 @@ $(document).ready(function() {
 		this.board = board;
 		this.board.bars.push(this);
 		this.kind = "rectangle";
-		this.speed = 10;
+		this.speed = 20;
 	};
 
 	self.Bar.prototype = {
@@ -59,6 +66,31 @@ $(document).ready(function() {
 		toString: function() {
 			return "x: "+this.x+" y: "+this.y;
 		},
+	};
+})();
+
+/**
+ * class Ball
+ * @return {[type]} [description]
+ */
+(function(){
+	self.Ball = function(x, y, radio, board) {
+		this.board = board;
+		this.x = x;
+		this.y = y;
+		this.radio = radio;
+		this.speed_x = 25;
+		this.speed_y = 0;
+		board.ball = this;
+		this.kind = "circle";
+		this.direction = 1;
+	};
+
+	self.Ball.prototype = {
+		move: function() {
+			this.x += (this.speed_x * this.direction);
+			this.y += (this.speed_y); 
+		}
 	};
 })();
 
@@ -82,34 +114,49 @@ $(document).ready(function() {
 				var el = this.board.elements[i];
 				drawElement(this.ctx, el);
 			}
+		},
+		play: function() {
+			if (this.board.playing) {
+				this.clean();
+				this.draw();
+				this.board.ball.move();
+			}
 		}
 	};
 
 	//method to draw elements in canvas
 	function drawElement(ctx, element) {
-		//if (element !== null && element.hasOwnProperty("kind")) {
 		switch(element.kind) {
 			case "rectangle":
 				ctx.fillRect(element.x, element.y, element.width, element.height);
 				break;
-		}	
-		//}
+			case "circle":
+				ctx.beginPath();
+				ctx.arc(element.x, element.y, element.radio, 0, 7);
+				ctx.fill();
+				ctx.closePath();
+				break;
+		}
 	}
 })();
 
 //global vars
 //const state = false;
-var board = new Board(800, 500);
-var bar = new Bar(20, 150, 40, 150, board);
-var bar_2 = new Bar(700, 150, 40, 150, board);
+var board = new Board(800, 550);
+var bar = new Bar(20, 150, 40, 170, board);
+var bar_2 = new Bar(740, 150, 40, 170, board);
 var canvas_Id = $('#canvas')[0];
 var board_view = new BoardView(canvas_Id, board);
-
+var ball = new Ball(350, 100, 10, board);
+board_view.draw();
 //self.addEventListener("load", controller);
 window.requestAnimationFrame(controller);
 
+/**
+ * [controller description]
+ * @return {[type]} [description]
+ */
 function controller(){
-	board_view.clean();
-	board_view.draw();
+	board_view.play();
 	window.requestAnimationFrame(controller);
 }
